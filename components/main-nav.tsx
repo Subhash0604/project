43841fa8cbe1a521962cd04e195caba577369371
Subhash0604 <subhashchandra26545@gmail.com>
2025-02-21 11:00:20 +0530

    route fix

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -30,7 +30,7 @@ export function MainNav() {
                   <li>
                     <NavigationMenuLink asChild>
                       <Link
-                        href="/search-rides"
+                        href="/rides"
                         className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
                       >
                         <div className="text-sm font-medium leading-none">Search Rides</div>
@@ -100,7 +100,7 @@ export function MainNav() {
             </Link>
           </Button>
           <Button size="sm" asChild>
-            <Link href="/register">Sign Up</Link>
+            <Link href="/signup">Sign Up</Link>
           </Button>
         </div>
       </div>
